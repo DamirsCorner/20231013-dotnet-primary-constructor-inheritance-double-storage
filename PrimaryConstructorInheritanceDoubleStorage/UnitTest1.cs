596b61cc13d2d649c2a51b74fc8c974fbc614840
Damir Arh <damir.arh@gmail.com>
2023-09-30 12:19:04 +0200

    Add project files.

---
+++ PrimaryConstructorInheritanceDoubleStorage/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PrimaryConstructorInheritanceDoubleStorage;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
